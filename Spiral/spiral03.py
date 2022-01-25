58d5622dfa3e8855936f0c17d841c85e0e39478b
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2022-01-25 10:25:46 -0400

    Update spiral03.py

--- Spiral/spiral03.py
+++ Spiral/spiral03.py
@@ -1,13 +1,13 @@
 import turtle
 
-colors = ['red', 'yellow', 'green', 'purple']
+colors = ['red', 'yellow', 'green', 'purple'] # Para usar 3 cores no triângulo deixe só 3 cores
 
 t = turtle.Pen()
 t.speed(10)
 turtle.bgcolor("black")
 
 for x in range(500):
-    t.pencolor(colors[x%4])
+    t.pencolor(colors[x%4]) # Para usar 3 cores no triângulo use "t.pencolor(colors[x%3])"
     t.forward(2*x)
-    t.left(90)
+    t.left(90) # Para formar um triângulo use "t.left(120)"
 turtle.done()
