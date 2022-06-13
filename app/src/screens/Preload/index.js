0db2743d2b7925b471cfc0379b0fc51a405b6597
joaoygorr <82791430+joaoygorr@users.noreply.github.com>
2022-06-13 18:10:02 -0400

    preload

--- app/src/screens/Preload/index.js
+++ app/src/screens/Preload/index.js
@@ -1,11 +1,30 @@
-import React from "react";
-import { Text } from "react-native";
-import { Container } from "./styles";
+import React, { useEffect } from "react";
+import { Container, LoadingIcon } from "./styles";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
+
+// Logo
+// import TeacherLogo from "../../assets/email.svg";
 
 export default () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem("token");
+      if (token !== null) {
+        // validar o token
+      } else {
+        navigation.navigate("SignIn");
+      }
+    };
+    checkToken();
+  }, []);
+
   return (
     <Container>
-      <Text>Preload</Text>
+      {/* <TeacherLogo width={100} height={160} /> */}
+      <LoadingIcon size="large" color="#FFFFFF" />
     </Container>
   );
 };
