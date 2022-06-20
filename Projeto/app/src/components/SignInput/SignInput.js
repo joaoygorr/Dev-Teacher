c8e3de3afcd8213efcc948690822fbdceb0aaca4
joaoygorr <82791430+joaoygorr@users.noreply.github.com>
2022-06-20 12:00:21 -0400

    Ajuste no projeto

--- Projeto/app/src/components/SignInput/SignInput.js
+++ Projeto/app/src/components/SignInput/SignInput.js
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 //components
-import { InputArea, Input } from "./SignInput.style";
+import {InputArea, Input} from './SignInput.style';
 
-export default ({ IconSvg, placeholder, value, onChangeText, password }) => {
+export default ({IconSvg, placeholder, value, onChangeText, password}) => {
   return (
     <InputArea>
-      <IconSvg />
+      <IconSvg width="24" height="24" fill="#268596" />
       <Input
         secureTextEntry={password}
         placeholder={placeholder}
